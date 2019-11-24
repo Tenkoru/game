2c32121be9a14d1f8ec2ff92f30ccd196a62c891
Александр Стрелец <astrelets@inbox.ru>
2019-11-24 13:27:42 +0300

    add app window and resize

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
-  type = "canvas"
+function initPixi() {
+    function scaleToWindowFunction() {
+        var scale = scaleToWindow(app.renderer.view, "gray");
+    }
+    let type = "WebGL";
+    if (!PIXI.utils.isWebGLSupported()) {
+        type = "canvas";
+    }
+
+    // Создать приложение с заданой шириной и высотой
+    let app = new PIXI.Application({ width: 800, height: 600 });
+
+    // Добавить приложение к странице
+    document.body.appendChild(app.view);
+
+    // Растянуть приложение на весь экран и перезапускать растяжку при ресайзе
+    scaleToWindowFunction();
+    window.addEventListener("resize", scaleToWindowFunction);
 }
 
-PIXI.utils.sayHello(type)
+initPixi();
+
